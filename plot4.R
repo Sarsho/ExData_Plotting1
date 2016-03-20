c0655aaf7c8e8bbd206bfd072cf6731337f59e57
Austin Overman <aoverman@cox.net>
2016-03-20 15:10:20 -0700

    Final commit for plot4.R and add plot4.png
    This is the final version for plot4.R generating four plots using
    par(mfrows = 2,2), these are essentially plot2 and plot3 with two
    additional variables

--- plot4.R
+++ plot4.R
@@ -93,7 +93,7 @@ tail(power_data[,10])
 ## first have to define the x-axis date range as days of the week
 ## this does the trick but forces the day names in the programatic tick locations
 ## I know there is a better way, just not able to get it to work
-png(filename = "Plot3.png", width = 480, height = 480)
+png(filename = "Plot4.png", width = 480, height = 480)
 
 par(mfrow = c(2,2))
 plot(power_data$long_date, power_data$Global_active_power, xaxt = "n", type = "l", 
@@ -106,18 +106,21 @@ plot(power_data$long_date, power_data$Voltage, xaxt = "n", type = "l",
 r <- as.POSIXct(round(range(power_data$long_date), "days"))
 axis.POSIXct(1, at=seq(r[1], r[2], by="days"), format="%d", labels = c("Thu", "Fri", "Sat"))
 
-plot(power_data$long_date, power_data$Sub_metering_3, xaxt = "n", type = "l", 
-     xlab = "", ylab = "Global Active Power (kilowatts)", col = "blue")
+plot(power_data$long_date, power_data$Sub_metering_1, xaxt = "n", type = "l", 
+       xlab = "", ylab = "Energy sub metering", col = "red")
+lines(power_data$long_date, power_data$Sub_metering_2, type = "l", col = 'yellow')
+lines(power_data$long_date, power_data$Sub_metering_3, type = "l", col = 'blue')
 r <- as.POSIXct(round(range(power_data$long_date), "days"))
 axis.POSIXct(1, at=seq(r[1], r[2], by="days"), format="%d", labels = c("Thu", "Fri", "Sat"))
-legend("topright", legend = "Sub_metering_3", bty = "n", lty = 1, col = "blue")
+legend("topright", legend = c("sub_metering_1", "sub_metering_2", "sub_metering_3"), 
+       bty = "n", cex = .5, lty = 1, col = c("red", "yellow", "blue"))
 
 plot(power_data$long_date, power_data$Global_reactive_power, xaxt = "n", type = "l", 
      xlab = "datetime", ylab = "Voltage")
 r <- as.POSIXct(round(range(power_data$long_date), "days"))
 axis.POSIXct(1, at=seq(r[1], r[2], by="days"), format="%d", labels = c("Thu", "Fri", "Sat"))
+par(mfrows = c(1,1))
 
 dev.off()
 
-## returning working directory to the original directory
 setwd(owd)
